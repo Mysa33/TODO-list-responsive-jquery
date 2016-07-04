e9dfba58155d14f4f08e2c6e660c99492e0f0b35
Mysa33 <tpdrn.france@gmail.com>
2016-07-04 20:03:25 +0200

    MAJ 04/07/16
    V0

--- js/inputHandler.js
+++ js/inputHandler.js
@@ -1,16 +1,14 @@
 var i = 0;
-
 // Get local storage
 function getStorage(){
     for( i = 0; i<localStorage.length; i++){
-        $("#taches").append("<li id='btn-"+i+"' class = 'list-group-item'>" + localStorage.getItem('tache-'+i) +" <button id='btn-"+i+"'><i class='fa fa-times'></i>Effacer</button></li>");
-        $("#taches li").addClass("list-group");// On ajoute la classe Bootstrap.
-        $("#taches li button").addClass("btn btn-primary");// On ajoute la classe Bootstrap.
+        $("#taches").append("<li id='btn-"+i+"' class = 'list-group-item'>"+localStorage.getItem('tache-'+i) +" <button id='btn-"+i+"'><i class='fa fa-times'></i>Effacer</button></li>");
+        $("#taches li").addClass("list-group");
+        $("#taches li button").addClass("btn btn-primary");
     }    
 }
 // Set local storage
-function setStorage(){
-    
+function setStorage(){    
     var taskVal = $("#tache").val();
     if (taskVal != "" ) {
         localStorage.setItem( "tache-"+i, taskVal );
@@ -18,21 +16,18 @@ function setStorage(){
     }     
 }
 // Get task/ Delete 
-function getTaskStorageID(){
-    
+function getStorageID(){
     var elemID = this.id;
     var reg  =   /[0-9]/g;
     var numberInID = reg.test ( elemID );
-    
     if(numberInID == true){
           //Get int in ID    
           var re1='.*?';	
           var re2='([-+]\\d+)';	
           var p = new RegExp(re1+re2,["i"]);
           var m = p.exec(elemID);
-          
           if (m != null){
-              var signed_int1=m[1];
+              var signed_int1=m[1];//
               var intID = signed_int1.replace(/</,"&lt;");
               var taskIndex = 'tache'+signed_int1;
               $('#'+elemID).remove();
@@ -45,7 +40,7 @@ function getTaskStorageID(){
 $(document).ready(function() {
     getStorage();
     $("#btnOne").on("click", setStorage);
-    $("button").on("click", getTaskStorageID);
+    $("button").on("click", getStorageID);
     $("#btnTwo").on("click", function(){localStorage.clear();location.reload();});
 });
 
