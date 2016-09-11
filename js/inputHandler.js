c6f12e23a573ca1bfd0b4cd4029ef7d20f48c92e
Mysa33 <tpdrn.france@gmail.com>
2016-09-11 18:35:27 +0200

    bug fixing

--- js/inputHandler.js
+++ js/inputHandler.js
@@ -1,20 +1,24 @@
 var i = 0;
 // Get local storage
 function getStorage(){
-    for( i = 0; i<localStorage.length; i++){
-        $("#taches").append("<li id='btn-"+i+"' class = 'list-group-item'>"+localStorage.getItem('tache-'+i) +" <button id='btn-"+i+"'><i class='fa fa-times'></i>Effacer</button></li>");
+	for ( i = 0; i < localStorage.length; ++i ) {
+		tacheKey = localStorage.key( i );
+		$("#taches").append("<li class = 'list-group-item'>"+localStorage.getItem(tacheKey) +" <button id='"+tacheKey+"'><i class='fa fa-times'></i>Effacer</button></li>");
         $("#taches li").addClass("list-group");
-        $("#taches li button").addClass("btn btn-primary");
-    }    
+        $("#taches li button").addClass("btn btn-primary my-btn");
+	}
+
 }
+
 // Set local storage
 function setStorage(){    
     var taskVal = $("#tache").val();
     if (taskVal != "" ) {
         localStorage.setItem( "tache-"+i, taskVal );
-        location.reload();
-
-    }     
+		location.reload();
+		
+    }
+	
 }
 // Get task/ Delete 
 function getStorageID(){
@@ -32,9 +36,10 @@ function getStorageID(){
               var signed_int1=m[1];//
               var intID = signed_int1.replace(/</,"&lt;");
               var taskIndex = 'tache'+intID;
+			  alert();
               $('#'+taskIndex).remove();
               localStorage.removeItem(taskIndex);
-              location.reload();
+			  location.reload();	
           }    
     }
 }
@@ -42,7 +47,7 @@ function getStorageID(){
 $(document).ready(function() {
     getStorage();
     $("#btnOne").on("click", setStorage);
-    $("button").on("click", getStorageID);
+    $(".my-btn").on("click", getStorageID);
     $("#btnTwo").on("click", function(){localStorage.clear();location.reload();});
 });
 
